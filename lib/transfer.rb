d76aa84c27c8a168964e3021c19528c120ae16e5
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-30 02:56:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Transfer
   attr_accessor :sender, :receiver, :transfer, :amount, :status
   
@@ -8,11 +9,16 @@ class Transfer
     @status = "pending"
   end
   
+<<<<<<< HEAD
+=======
+  
+>>>>>>> 8d3633fd9dd8d13ea29a047a2d4f28374541d145
   def valid?
     @sender.valid? && @receiver.valid?
   end
   
   def execute_transaction
+<<<<<<< HEAD
     if self.valid? == false || @sender.balance < amount
       @status = "rejected"
         "Transaction rejected. Please check your account balance."
@@ -30,6 +36,18 @@ class Transfer
         @receiver.balance -= amount
         @status = "reversed"
     end
+=======
+      if @status != "complete" && @sender.valid?
+        
+        @sender.balance -= amount
+        @receiver.balance += amount
+        @status = "complete"
+        
+      elsif
+        "Transaction rejected. Please check your account balance."
+      end
+      
+>>>>>>> 8d3633fd9dd8d13ea29a047a2d4f28374541d145
   end
   
 end
